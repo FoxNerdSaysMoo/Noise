291599b759c912ad0f829b884fc3749413bf42d1
Zeb <65097669+FoxNerdSaysMoo@users.noreply.github.com>
2020-12-10 09:24:44 -0600

    Move static() to noise.py

--- noise.py
+++ noise.py
@@ -1,6 +1,15 @@
 import numpy as np
 from utils import static
 from scipy.ndimage.filters import gaussian_filter
+from random import randint
+
+
+def static(dims):
+    result = np.zeros(dims)
+    for x in range(dims[0]):
+        for y in range(dims[1]):
+            result[x, y] = randint(0, 255)
+    return result
 
 
 def levellednoise(
